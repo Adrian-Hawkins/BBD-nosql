03264488a2e17b5fb8e1db9963eada53442296c0
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-07-21 19:26:06 +0200

    format

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -3,30 +3,30 @@ module "networking" {
 }
 
 module "ecs" {
-  source = "./modules/ecs"
-  vpc_id = module.networking.vpc_id
-  subnet_ids = module.networking.subnet_ids
-  db_username = var.db_username
-  db_password = var.db_password
-  efs_id = module.storage.efs_id
+  source            = "./modules/ecs"
+  vpc_id            = module.networking.vpc_id
+  subnet_ids        = module.networking.subnet_ids
+  db_username       = var.db_username
+  db_password       = var.db_password
+  efs_id            = module.storage.efs_id
   security_group_id = module.networking.security_group_id
-  target_group_arn = module.load_balancer.target_group_arn
+  target_group_arn  = module.load_balancer.target_group_arn
 }
 
 module "load_balancer" {
-  source = "./modules/load_balancer"
-  vpc_id = module.networking.vpc_id
+  source     = "./modules/load_balancer"
+  vpc_id     = module.networking.vpc_id
   subnet_ids = module.networking.subnet_ids
 }
 
 module "storage" {
-  source = "./modules/storage"
-  vpc_id = module.networking.vpc_id
+  source    = "./modules/storage"
+  vpc_id    = module.networking.vpc_id
   subnet_id = module.networking.subnet_ids[0]
 }
 
 module "backup" {
-  source = "./modules/backup"
+  source  = "./modules/backup"
   efs_arn = module.storage.efs_arn
 }
 
