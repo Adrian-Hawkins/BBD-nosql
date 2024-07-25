b556080e92a36770ee30d4d834530ec1404dc9af
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-07-25 09:31:22 +0200

    get order with key via email

--- src/commands/completeOrder.command.ts
+++ src/commands/completeOrder.command.ts
@@ -8,17 +8,28 @@ export class CompleteOrderCommand implements ICommand<Promise<void>,[orderId:str
 
     async execute(orderId:string){
         try {
-            const result = await DBPool.query(`
-                UPDATE "Orders"
-                SET "orderComplete" = true
+            await DBPool.query('BEGIN');
+            // Get all items in order items array
+            const orderResult: QueryResult<{orderItems: any[]}> = await DBPool.query(`
+                SELECT "orderItems"
+                FROM "Orders"
                 WHERE "orderId" = $1
             `, [orderId]);
-            console.log(result);
+            const total = orderResult.rows[0].orderItems.reduce((sum, item) => sum + item.price, 0);
+            console.log(total)
+            const result = await DBPool.query(`
+                UPDATE "Orders"
+                SET "orderComplete" = true,
+                    "totalPrice" = $1
+                WHERE "orderId" = $2
+            `, [total, orderId]);
 
             if (result.rowCount === 0) {
                 throw new Error(`Order with ID ${orderId} not found`);
             }
+            await DBPool.query('COMMIT');
         } catch (e) {
+            await DBPool.query('ROLLBACK');
             throw e;
         }
     }
--- src/queries/getOrderwith.query.ts
+++ src/queries/getOrderwith.query.ts
@@ -1,25 +1,26 @@
 import {IQuery} from "./IQuery";
 import {DBPool} from "../database";
 
-export class GetOrderWithQuery implements IQuery<Promise<any>,[customerId: number, key: string, value: any]>{
-    async execute(customerId: number, key: string, value: any){
+export class GetOrderWithQuery implements IQuery<Promise<any>,[email: string, key: string, value: any]>{
+    async execute(email: string, key: string, value: any) {
+        const valueType = typeof value === 'string' ? 'text' : 'jsonb';
         try {
-            const valueType = typeof value === 'string' ? 'text' : 'jsonb';
-
             const result = await DBPool.query(`
                 SELECT DISTINCT o."orderId", o."orderDate", o."totalPrice", o."orderItems", o."orderComplete"
                 FROM "Orders" o
+                         JOIN "Customers" c ON o."customerId" = c."customerId"
                          JOIN LATERAL (
                     SELECT item
                     FROM unnest(o."orderItems") AS item
                     ) AS expanded_items ON true
-                         JOIN "Hoodies" h ON (item->>'hoodieId')::INT = h."hoodieId"
-                WHERE o."customerId" = $1
+                         JOIN "Hoodies" h ON (item ->> 'hoodieId')::INT = h."hoodieId"
+                WHERE c."email" = $1
                   AND h."hoodieDetails" @> jsonb_build_object($2::text, $3::${valueType})
-            `, [customerId, key, value]);
+            `, [email, key, value]);
             return result.rows;
-        } catch(e) {
-            throw(e);
+        }
+        catch (e) {
+            throw e;
         }
     }
 }
